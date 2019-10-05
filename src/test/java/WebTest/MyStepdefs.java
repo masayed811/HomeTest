22ffb65b93a5d21b9de974c20b97e8f6c9f50eef
masayed811 <sayed811@gmail.com>
2019-10-05 14:43:28 +0100

    Hometest
    Signed-off-by: masayed811 <sayed811@gmail.com>

--- src/test/java/WebTest/MyStepdefs.java
+++ src/test/java/WebTest/MyStepdefs.java
@@ -1,74 +1,60 @@
 package WebTest;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class MyStepdefs {
+
+
+    HomePage homePage = new HomePage();
+    CreatAccount creatAccount = new CreatAccount();
+    ContractUs contractUs = new ContractUs();
+    AddToCart addToCart = new AddToCart();
+    RemoveFromCart removeFromCart = new RemoveFromCart();
+
     @cucumber.api.java.en.Given("^user is on home page$")
     public void userIsOnHomePage() {
     }
 
     @When("^user click on contactUs$")
-    public void user_click_on_contactUs() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_click_on_contactUs() {
     }
 
     @When("^user select customer service$")
-    public void user_select_customer_service() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_select_customer_service() {
     }
 
     @When("^user enter email address$")
-    public void user_enter_email_address() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_enter_email_address() {
     }
 
     @When("^user enter text message$")
-    public void user_enter_text_message() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_enter_text_message() {
     }
 
     @When("^user click on send button$")
-    public void user_click_on_send_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_click_on_send_button() {
     }
 
     @Then("^user can see message has been successfully sent$")
-    public void user_can_see_message_has_been_successfully_sent() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_can_see_message_has_been_successfully_sent() {
     }
 
     @When("^user click on singIn button$")
-    public void user_click_on_singIn_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_click_on_singIn_button() {
     }
 
     @When("^user enter  already register email id$")
-    public void user_enter_already_register_email_id() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_enter_already_register_email_id() {
     }
 
     @When("^user click on creat an account button$")
-    public void user_click_on_creat_an_account_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_click_on_creat_an_account_button() {
     }
 
     @Then("^user can see the error messaage$")
-    public void user_can_see_the_error_messaage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void user_can_see_the_error_messaage() {
     }
 
     @When("^user click on item$")
