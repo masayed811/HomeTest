d9bf171499af32e98ef8627122fcfe33c81e5acf
masayed811 <sayed811@gmail.com>
2019-10-07 15:37:03 +0100

    Hometest
    Signed-off-by: masayed811 <sayed811@gmail.com>

---
+++ target/cucumber-report/report.js
@@ -0,0 +1,382 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/Resources/Features/AddToCart.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Item add to cart",
+  "description": "",
+  "id": "item-add-to-cart",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@AddToCart"
+    }
+  ]
+});
+formatter.before({
+  "duration": 8261162068,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "User able to add an item to cart",
+  "description": "",
+  "id": "item-add-to-cart;user-able-to-add-an-item-to-cart",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "user is on home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "user click on item",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "user select size",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "User select color",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "user click on add to cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user can see the message Product successfully added to your shopping cart",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepdefs.userIsOnHomePage()"
+});
+formatter.result({
+  "duration": 107105672,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userClickOnItem()"
+});
+formatter.result({
+  "duration": 24328,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userSelectSize()"
+});
+formatter.result({
+  "duration": 19006,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userSelectColor()"
+});
+formatter.result({
+  "duration": 18246,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userClickOnAddToCart()"
+});
+formatter.result({
+  "duration": 15965,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userCanSeeTheMessageProductSuccessfullyAddedToYourShoppingCart()"
+});
+formatter.result({
+  "duration": 17867,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 84308464,
+  "status": "passed"
+});
+formatter.uri("src/test/Resources/Features/ContactUs.feature");
+formatter.feature({
+  "line": 3,
+  "name": "Contact Us",
+  "description": "",
+  "id": "contact-us",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@ContactUs"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6792614286,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "User should able send message to customer service",
+  "description": "",
+  "id": "contact-us;user-should-able-send-message-to-customer-service",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "user is on home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "user click on contactUs",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "user select customer service",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user enter email address",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "user enter text message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "user click on send button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "user can see message has been successfully sent",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepdefs.userIsOnHomePage()"
+});
+formatter.result({
+  "duration": 48657,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_click_on_contactUs()"
+});
+formatter.result({
+  "duration": 25849,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_select_customer_service()"
+});
+formatter.result({
+  "duration": 27369,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_enter_email_address()"
+});
+formatter.result({
+  "duration": 24328,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_enter_text_message()"
+});
+formatter.result({
+  "duration": 25469,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_click_on_send_button()"
+});
+formatter.result({
+  "duration": 22808,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_can_see_message_has_been_successfully_sent()"
+});
+formatter.result({
+  "duration": 25849,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 75937222,
+  "status": "passed"
+});
+formatter.uri("src/test/Resources/Features/CreatAccount.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Craete account",
+  "description": "",
+  "id": "craete-account",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@CraetAccount"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6935875725,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "User should able to creat an account",
+  "description": "",
+  "id": "craete-account;user-should-able-to-creat-an-account",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "user is on home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "user click on singIn button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "user enter  already register email id",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "user click on creat an account button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "user can see the error messaage",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepdefs.userIsOnHomePage()"
+});
+formatter.result({
+  "duration": 41054,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_click_on_singIn_button()"
+});
+formatter.result({
+  "duration": 20527,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_enter_already_register_email_id()"
+});
+formatter.result({
+  "duration": 15965,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_click_on_creat_an_account_button()"
+});
+formatter.result({
+  "duration": 14445,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.user_can_see_the_error_messaage()"
+});
+formatter.result({
+  "duration": 13685,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 2120696491,
+  "status": "passed"
+});
+formatter.uri("src/test/Resources/Features/RemoveFromCart.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Item remove from cart",
+  "description": "",
+  "id": "item-remove-from-cart",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@RemoveFromCart"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6144071878,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "User able to remove an item from cart",
+  "description": "",
+  "id": "item-remove-from-cart;user-able-to-remove-an-item-from-cart",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "user is on home page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "user click on cart",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "user click on delete",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "Item delete  successfully from cart",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepdefs.userIsOnHomePage()"
+});
+formatter.result({
+  "duration": 39533,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userClickOnCart()"
+});
+formatter.result({
+  "duration": 17486,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepdefs.userClickOnDelete()"
+});
+formatter.result({
+  "duration": 23568,
+  "status": "passed"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.after({
+  "duration": 2109779893,
+  "status": "passed"
+});
+});
